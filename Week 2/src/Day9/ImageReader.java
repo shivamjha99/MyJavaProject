839114484e4bc7e451cf6de27de12e584922630f
shivamjha99 <shivamjha129@gmail.com>
2021-08-06 00:23:36 +0530

    Day 9

---
+++ Week 2/src/Day9/ImageReader.java
@@ -0,0 +1,36 @@
+package Day9;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ImageReader {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		try {
+			
+		
+   FileInputStream in= new FileInputStream("D:\\BramhaNandam.jpg");
+   FileOutputStream o = new FileOutputStream("D:\\new.jpg");
+   BufferedInputStream B=new BufferedInputStream(in);
+   BufferedOutputStream Bo= new BufferedOutputStream(o);
+   
+   int a =0;
+   while(a!=-1) {
+	   a=B.read();
+	   Bo.write(a);
+   }
+   B.close();
+   Bo.close();
+		}
+		 catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	}
+
+}
